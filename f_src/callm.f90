b8776a5459ec1a763492b7d938705fbb0403d25e
Jaewook Lee <jaewooklee042@gmail.com>
2024-01-12 16:57:32 +0100

    add CallM

---
+++ f_src/callm.f90
@@ -0,0 +1,13 @@
+module CallM
+  use, intrinsic :: ISO_C_BINDING
+  implicit none
+
+  interface
+
+  subroutine call_py() bind (c, name="callme")
+    import
+  end subroutine call_py
+
+  end interface
+
+end module CallM
